e0b3b3ec1a490b44823fe088dfd1ec7c94c66a13
Hector Perez <hector.perez.m@hotmail.com>
2026-01-01 15:32:26 -0300

    feat: plugin loads succesfully

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function main() {
+  logseq.UI.showMsg("Hello from Booker! 🚀", "success");
+}
+
+logseq.ready(main).catch(console.error);
---
+++ src/logseq.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="@logseq/libs" />
+
+declare global {
+  const logseq: typeof import("@logseq/libs");
+}
+
+export {};
+
